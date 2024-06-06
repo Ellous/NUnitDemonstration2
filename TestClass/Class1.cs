4dbfdaba1bd7cbf4104c7ff9ec30925cbfda2e00
HP <HP@ELLOUS>
2024-06-05 22:15:11 -0700

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using NUnitDemonstration2;
+
+namespace TestClass
+
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Mohan Test
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+            int firstAngle = 70;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
